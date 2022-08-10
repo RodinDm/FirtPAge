ef5d99f43cc241024d8a4ec2732fdff6e42c2bea
Dmitriy <thed1@rambler.ru>
2022-08-10 15:04:43 +0300

    апдате

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.5 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
 //
 // Scripts
 // 
